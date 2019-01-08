931127a112e06f009e7db7b1c566b2ddcb6ac421
Alexandre Hamez <alexandre.hamez@easymile.com>
2019-01-08 14:55:28 +0100

    Add a load balancer

--- config/prod.exs
+++ config/prod.exs
@@ -1,14 +1,14 @@
 use Mix.Config
 
 config :todo,
-  http_port: 8080
+  http_port: 9090
 
 config :libcluster,
   topologies: [
     k8s_example: [
       strategy: Elixir.Cluster.Strategy.Kubernetes.DNS,
       config: [
-        service: "todo-service-headless",
+        service: "todo-headless",
         application_name: "todo",
         polling_interval: 3_000
       ]
